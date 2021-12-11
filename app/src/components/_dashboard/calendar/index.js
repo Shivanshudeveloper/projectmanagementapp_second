5a456296e0f71435b63caf28cd4744540e9f2169
Shivanshu Gupta <40379768+Shivanshudeveloper@users.noreply.github.com>
2021-12-11 14:16:24 +0530

    Work

--- app/src/components/_dashboard/calendar/index.js
+++ app/src/components/_dashboard/calendar/index.js
@@ -1,3 +1,4 @@
 export { default as CalendarForm } from './CalendarForm';
 export { default as CalendarToolbar } from './CalendarToolbar';
 export { default as CalendarStyle } from './CalendarStyle';
+export { default as CalendarStyle2 } from './CalendarStyle2';
